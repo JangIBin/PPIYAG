16173141f458a70bda84ccc7de2c49395e448db3
JangIBin <wkddlqls321@gmail.com>
2021-03-23 14:58:30 +0900

    branch test

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Test = () => {
+    return (
+        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>React Native Test</Text>
+        </View>
+    )
+}
+
+export default Test;
